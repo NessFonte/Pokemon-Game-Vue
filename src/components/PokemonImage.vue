f44341c1efc63c5f141fcddae3880ab74b5a05bf
neisser <neisser2909@gmail.com>
2023-03-06 14:05:15 -0500

    Agregadas las imégenes del Pokémon en el componenete PokemonImage.vue

--- src/components/PokemonImage.vue
+++ src/components/PokemonImage.vue
@@ -1,5 +1,8 @@
 <template>
-  <h3>Pokemon image</h3>
+  <div class="pokemon-container">
+    <img class="pokemon-hidden" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+    <!--<img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">-->
+  </div>
 </template>
 
 <script>
@@ -8,6 +11,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+/* Pokemon Picture */
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    position: absolute;
+    right: 38%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.pokemon-hidden {
+    filter: brightness(0);
+}
 </style>
