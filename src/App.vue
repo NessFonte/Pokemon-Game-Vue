524a7c2173249e2f762a4edbc36bdd069db681ac
neisser <neisser2909@gmail.com>
2023-03-06 13:36:15 -0500

    Creados los archivos PokemonImage.vue y PokemonOptions.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <PokemonPage />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from './pages/PokemonPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokemonPage
   }
 }
 </script>
@@ -22,5 +24,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo{
+  padding-right: 1500px;
+  width: 100px;
 }
 </style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>¿Quién es este Pokémon?</h1>
+        <PokemonImage />
+        <PokemonOptions />
+    </div>
+</template>
+
+<script>
+import PokemonImage from '@/components/PokemonImage.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+    name: "Pokemon",
+    components: {
+        PokemonImage,
+        PokemonOptions
+    }
+    
+}
+</script>
